1bf403885c8548784af615021a77a104a0771011
Ahmad Taimur <ullahtaimur@gmail.com>
2025-06-22 12:04:00 +0500

    class 01

---
+++ class01.py
@@ -0,0 +1,11 @@
+# print("Hello World")
+# userName = "Ahmad Taimur"
+# userNameFull = "Ahmad Taimur"
+# user_name = "Ahmad Taimur"
+# UserName = "Ahmad Taimur"
+
+# num=1
+num = 10 * 20
+# print(userName)
+print(num)
+# print(id(userName))
