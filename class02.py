1a25dfcf8d02c5c930c782c59a71b6bda33f0755
Ahmad Taimur <ullahtaimur@gmail.com>
2025-06-23 12:50:00 +0500

    class 02

--- class02.py
+++ class02.py
@@ -7,3 +7,11 @@ print("textData",textData)
 booleanData= False
 booleanData1= True
 print("booleanData",booleanData)
+# operators
+num1 = 10
+num2 = 20
+
+print("Addition", num1 + num2)
+print("Subtraction", num2 - num1)   
+print("Multiplication", num1 * num2)
+print("Division", num2 / num1)  # always return float value
