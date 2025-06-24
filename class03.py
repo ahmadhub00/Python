0a6d2e24ca7e9e87e27855ec2bac16f29804ff1d
Ahmad Taimur <ullahtaimur@gmail.com>
2025-06-24 12:04:00 +0500

    class 03

--- class03.py
+++ class03.py
@@ -6,11 +6,18 @@ num3,num4 = 30,40
 result = num1 ** num2 # for exponnentiation
 
 result1 = num3 // num4 # for floor division
-print(result)
-print(result1)
+# print(result)
+# print(result1)
 
 # Operators precedence
 # - ()
 # - **
 # - / * // %
 # - + -
+
+result3 = num1 == num2 # for comparison
+result4 = num1 != num2
+result5 = num1 > num2
+#answer will be in boolean true or false
+print(result3)
+print(type(result3))
