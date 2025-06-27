1db07adf0a5b0117222730c8b45098cc124a2bca
Ahmad Taimur <ullahtaimur@gmail.com>
2025-06-27 12:04:00 +0500

    class 04

---
+++ class04.py
@@ -0,0 +1,11 @@
+print ("line1")
+print ("line2")
+def Hide():  #def hides the code inside it
+  print ("line3") 
+  print ("line4")
+print ("line5")
+Hide()  #calling the function to print line3 and line4
+print ("line6")
+
+
+    
