50793de2fd333d05ba162f6e8800cc75468c25f5
Ahmad Taimur <ullahtaimur@gmail.com>
2025-06-30 12:04:00 +0500

    class 04

--- class04.py
+++ class04.py
@@ -1,11 +1,17 @@
-print ("line1")
-print ("line2")
-def Hide():  #def hides the code inside it
-  print ("line3") 
-  print ("line4")
-print ("line5")
-Hide()  #calling the function to print line3 and line4
-print ("line6")
+# print ("line1")
+# print ("line2")
+# def Hide():  #def hides the code inside it
+#   print ("line3") 
+#   print ("line4")
+# print ("line5")
+# Hide()  #calling the function to print line3 and line4
+# print ("line6")
 
+# call function 
+def Add(c,d,e=5):  #default value of e is 100
+  a = 1
+  b = 2
+  c = a + b + c + d + e
+  print (c)
 
-    
+Add(3,4)
