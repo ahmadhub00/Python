65a09bd0bd9d1a688feeb25b3b63ab76d9b7083e
Ahmad Taimur <ullahtaimur@gmail.com>
2025-07-04 12:04:00 +0500

    class 06

--- class06.py
+++ class06.py
@@ -3,3 +3,9 @@ if isGreater:
     print("50 is greater than 10")
 
 # if/else only works with boolean values
+
+num1 = int(input("Enter a number: "))
+if num1 % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
