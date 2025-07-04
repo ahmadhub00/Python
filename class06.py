1c864750097bc7de0af79a8df6228bc1a8467c27
Ahmad Taimur <ullahtaimur@gmail.com>
2025-07-04 12:04:00 +0500

    class 06

--- class06.py
+++ class06.py
@@ -26,6 +26,18 @@ else:
 num3 = int (input("Enter a number: "))
 isEven = num3 % 2 == 0
 if isEven:
-        print("Even number")
-        if num3 > 50:
-            print("Number is greater than 50:", num3)
+    print("Even number")
+    if num3 > 50:
+        print("Number is greater than 50:", num3)
+
+# Write a program that takes the user input of three digits and checks the largest number.
+num4 = int(input("Enter first number: "))
+num5 = int(input("Enter second number: "))  
+num6 = int(input("Enter third number: "))
+
+if num4 > num5 and num4 > num6:
+    print("Largest number is:", num4)
+elif num5 > num4 and num5 > num6:
+    print("Largest number is:", num5)
+else:
+    print("Largest number is:", num6)
